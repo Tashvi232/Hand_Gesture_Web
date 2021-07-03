ff69112b15096735e4fe84817fca5e451bb3616f
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2021-07-03 19:51:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+gesture="";
 Webcam.set({
     width:350,height:300,image_format:"png",png_quality:90
 });
@@ -10,7 +11,49 @@ function take_snapshot() {
 
 }
 console.log("ml5 version",ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/uRaC0bRUD/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/KmT1TPax3/model.json",modelLoaded);
 function modelLoaded(){
 console.log("Model Loaded");
 }
+
+function check() {
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+    }
+
+    function gotResult(error,results){
+        if(error){console.error(error);}
+        else{
+            console.log(results);
+            document.getElementById("result_emotion_name").innerHTML=results[0].label;
+            
+            
+            gesture=results[0].label
+            speak()
+            if(results[0].label=="OK"){
+                document.getElementsById("update_emoji").innerHTML="&#128076;";
+            }
+            if(results[0].label=="Thumbs up"){
+                document.getElementById("update_emoji").innerHTML="&#128077";
+            }
+            if(results[0].label=="Two/Peace/Victory"){
+                document.getElementById("update_emoji").innerHTML="&#9996";
+            }
+            if(results[0].label=="Raised fist"){
+                document.getElementById("update_emoji").innerHTML="&#9994";
+            }
+            if(results[0].label=="Vulcan Salute"){
+                document.getElementById("update_emoji").innerHTML="&#128406";
+            } 
+            if(results[0].label=="Sign of horns"){
+                document.getElementById("update_emoji").innerHTML="&#129304";
+            }
+        }
+    }
+    function speak() {
+        var synth=window.spaceSynthesis;
+        speak_data_1=gesture;
+        
+        var utterThis=new SpeechSynthesisUtterance(speak_data_1);
+        synth.speak(utterThis);
+        }
